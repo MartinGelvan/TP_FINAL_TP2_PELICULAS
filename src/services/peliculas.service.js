f1cb79b4819349ed6e5a59a2bd2bb386a323872b
Lucas <lucas-aupi@hotmail.com>
2024-11-09 21:45:33 -0300

    remove comments

--- src/services/peliculas.service.js
+++ src/services/peliculas.service.js
@@ -1,37 +1,31 @@
-import axios from 'axios'; // Importar axios para hacer solicitudes HTTP
+import axios from 'axios'; 
 import PeliculasFactory from "../models/peliculas.factory.js"; // Tu fábrica de persistencia
-import server from "../server.js"; // Cargar la configuración del servidor
+import server from "../server.js";
 
 class PeliculasServices {
   constructor() {
-    // Usar la fábrica para persistencia según el valor en el servidor
     this.peliculasFactory = PeliculasFactory.get(server.PERSISTENCE);
   }
 
-  // Este método obtiene las películas de TMDb
   getPeliculas = async () => {
-    // Si la persistencia es en memoria o en otro almacenamiento, usar la fábrica:
     if (server.PERSISTENCE !== "DB") {
       return await this.peliculasFactory.getPeliculas();
     }
 
-    // Si la persistencia es en base de datos, obtén las películas de TMDb
     try {
       const page = 1; // Número de página para paginación
       const language = 'es'; // Idioma de las películas (español)
       const apiKey = process.env.TMDB_API_KEY; // Asegúrate de tener la clave de API en el archivo .env
 
-      // Realizar la solicitud a la API de TMDb para obtener películas populares
       const response = await axios.get('https://api.themoviedb.org/3/movie/popular', {
         params: {
-          api_key: apiKey,  // Usamos la clave de API
-          page: page,       // Número de página para los resultados
-          language: language // Idioma de las películas
+          api_key: apiKey,  
+          page: page,      
+          language: language 
         }
       });
 
-      // Devolver los resultados obtenidos desde TMDb
-      return response.data.results;  // Los resultados contienen las películas
+      return response.data.results;  
     } catch (error) {
       console.error('Error al obtener las películas desde TMDb:', error);
       throw new Error('Error al obtener las películas desde TMDb');
